b9fb0b4bdb87580c09cc39da93c60e0775ec3d76
David Jensen <tsourdox@icloud.com>
2023-10-13 14:00:22 +0200

    add cases for pending & rejected + better types

--- store/userSlice.ts
+++ store/userSlice.ts
@@ -1,19 +1,22 @@
 import {
   PayloadAction,
-  createAsyncThunk,
   createSlice,
 } from "@reduxjs/toolkit";
+import { createAppAsyncThunk } from "./utils";
 
 interface UserState {
   name: string;
   savingsGoal: number;
   isNameDay: boolean;
+  loading: "idle" | "pending";
+  errorMessage?: string;
 }
 
 const initialState: UserState = {
   name: "My",
   savingsGoal: 100,
   isNameDay: false,
+  loading: "idle",
 };
 
 const userSlice = createSlice({
@@ -28,27 +31,39 @@ const userSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
+    builder.addCase(setName.pending, (state, action) => {
+      state.loading = "pending";
+    });
     builder.addCase(setName.fulfilled, (state, action) => {
       state.name = action.payload.name;
       state.isNameDay = action.payload.isNameDay;
+      state.loading = "idle";
+    });
+    builder.addCase(setName.rejected, (state, action) => {
+      state.loading = "idle";
+      state.errorMessage = action.payload;
     });
   },
 });
 
-export const setName = createAsyncThunk<
+export const setName = createAppAsyncThunk<
   {
     name: string;
     isNameDay: boolean;
   },
   string
 >("user/setName", async (name, thunkAPI) => {
-  const response = await fetch(
-    "https://sholiday.faboul.se/dagar/v2.1/2023/10/13"
-  );
-  const result = await response.json();
-  const names = result.dagar[0].namnsdag as string[];
-
-  return { name, isNameDay: names.includes(name) };
+  try {
+    const response = await fetch(
+      "https://sholiday.faboul.se/dagar/v2.1/2023/10/13"
+    );
+    const result = await response.json();
+    const names = result.dagar[0].namnsdag as string[];
+    return { name, isNameDay: names.includes(name) };
+  } catch (error: any) {
+    console.error(error);
+    return thunkAPI.rejectWithValue(error.message);
+  }
 });
 
 export const { setSavingsGoal } = userSlice.actions;
