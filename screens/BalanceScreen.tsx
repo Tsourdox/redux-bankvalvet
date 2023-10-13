174092bf17bdcd4c37615816e77f0b998cb2fb7d
David Jensen <tsourdox@icloud.com>
2023-10-13 13:48:15 +0200

    add redux thunk for setName and fetch if it is users nameday

--- screens/BalanceScreen.tsx
+++ screens/BalanceScreen.tsx
@@ -10,9 +10,13 @@ import {
   useAppDispatch,
   useAppSelector,
 } from "../store/store";
-import { setSavingsGoal } from "../store/userSlice";
+import {
+  setName,
+  setSavingsGoal,
+} from "../store/userSlice";
 
 export default function BalanceScreen() {
+  const [name, setNameInternal] = useState("");
   const [goal, setGoal] = useState(0);
   const dispatch = useAppDispatch();
 
@@ -32,6 +36,10 @@ export default function BalanceScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Welcome: {user.name}</Text>
       <Text style={styles.subTitle}>
+        It is {!user.isNameDay && "not"} your name day
+        today!
+      </Text>
+      <Text style={styles.subTitle}>
         Your goal is set to: {user.savingsGoal} kr
       </Text>
       <Text style={styles.subTitle}>
@@ -40,6 +48,16 @@ export default function BalanceScreen() {
           : `You have: ${leftToGoal} kr to reach your goal`}
       </Text>
       <Text>Balance: {balance} kr</Text>
+
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setNameInternal}
+      />
+      <Button
+        title="Set Name"
+        onPress={() => dispatch(setName(name))}
+      />
 
       <TextInput
         style={styles.input}
--- store/userSlice.ts
+++ store/userSlice.ts
@@ -1,25 +1,25 @@
 import {
   PayloadAction,
+  createAsyncThunk,
   createSlice,
 } from "@reduxjs/toolkit";
 
 interface UserState {
   name: string;
   savingsGoal: number;
+  isNameDay: boolean;
 }
 
 const initialState: UserState = {
   name: "My",
   savingsGoal: 100,
+  isNameDay: false,
 };
 
 const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setName: (state, action: PayloadAction<string>) => {
-      state.name = action.payload;
-    },
     setSavingsGoal: (
       state,
       action: PayloadAction<number>
@@ -27,8 +27,29 @@ const userSlice = createSlice({
       state.savingsGoal = action.payload;
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(setName.fulfilled, (state, action) => {
+      state.name = action.payload.name;
+      state.isNameDay = action.payload.isNameDay;
+    });
+  },
 });
 
-export const { setName, setSavingsGoal } =
-  userSlice.actions;
+export const setName = createAsyncThunk<
+  {
+    name: string;
+    isNameDay: boolean;
+  },
+  string
+>("user/setName", async (name, thunkAPI) => {
+  const response = await fetch(
+    "https://sholiday.faboul.se/dagar/v2.1/2023/10/13"
+  );
+  const result = await response.json();
+  const names = result.dagar[0].namnsdag as string[];
+
+  return { name, isNameDay: names.includes(name) };
+});
+
+export const { setSavingsGoal } = userSlice.actions;
 export const userReducer = userSlice.reducer;
