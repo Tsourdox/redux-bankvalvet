ce5a04c9b95897eb1061d73b1ac4c29dd834eb99
David Jensen <tsourdox@icloud.com>
2023-10-13 10:33:18 +0200

    add form to set new savings goal

--- screens/BalanceScreen.tsx
+++ screens/BalanceScreen.tsx
@@ -1,7 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
-import { useAppSelector } from "../store/store";
+import { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import {
+  useAppDispatch,
+  useAppSelector,
+} from "../store/store";
 
 export default function BalanceScreen() {
+  const [savingsGoal, setSavingsGoal] = useState(0);
+  const dispatch = useAppDispatch();
+
   const balance = useAppSelector(
     (state) => state.balance.balance
   );
@@ -27,6 +40,24 @@ export default function BalanceScreen() {
       </Text>
       <Text>Balance: {balance} kr</Text>
 
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={String(savingsGoal)}
+        onChangeText={(value) =>
+          setSavingsGoal(Number(value))
+        }
+      />
+      <Button
+        title="Set new goal"
+        onPress={() =>
+          dispatch({
+            type: "SET_SAVINGS_GOAL",
+            payload: savingsGoal,
+          })
+        }
+      />
+
       <Text>Transactions:</Text>
       {transactions.map((transaction, index) => (
         <Text key={index}>{transaction} kr</Text>
@@ -44,6 +75,12 @@ const styles = StyleSheet.create({
   subTitle: {
     fontSize: 18,
     marginBottom: 12,
+  },
+  input: {
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderColor: "black",
   },
   container: {
     flex: 1,
