7dba78a038013b4bb349628bcf6ce7ca0af0e529
David Jensen <tsourdox@icloud.com>
2023-10-13 11:44:30 +0200

    add store file with exported typings

---
+++ store/store.ts
@@ -0,0 +1,25 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  TypedUseSelectorHook,
+  useDispatch,
+  useSelector,
+} from "react-redux";
+
+const store = configureStore({
+  reducer: {
+    // balance: balanceReducer,
+    // user: userReducer
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch =
+  useDispatch;
+
+// Behövs den här längre?
+export const useAppSelector: TypedUseSelectorHook<RootState> =
+  useSelector;
+
+export default store;
